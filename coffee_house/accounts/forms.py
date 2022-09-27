e953192aeb09f02cf836b333bff1be4c6681f3d9
MidnightRoam <saldatsenka@list.ru>
2022-09-27 14:53:25 +0200

    Fixed some bugs with authorization and form and stylization was extended

--- coffee_house/accounts/forms.py
+++ coffee_house/accounts/forms.py
@@ -6,7 +6,20 @@ from .models import User
 
 class UserLoginForm(AuthenticationForm):
     """User sign in form"""
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "id": "email__signup",
+        "placeholder": "Enter your username"
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "id": "password__signup",
+        "placeholder": "Enter your password"
+    }))
 
     class Meta:
         model = User
         fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'shadow__input'
