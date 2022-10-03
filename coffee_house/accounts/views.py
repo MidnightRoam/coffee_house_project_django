f9608ecbcb60687b78f06b22b0de7aeacb6714c3
MidnightRoam <saldatsenka@list.ru>
2022-10-03 17:48:46 +0200

    Rewrite sign in view using generic.View, instead of generic.ListView. Also developed and stylized the output of erros in sign up form

--- coffee_house/accounts/views.py
+++ coffee_house/accounts/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.shortcuts import HttpResponseRedirect, render
+from django.views import View
 from django.views.generic import ListView, CreateView
 
 from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -12,12 +13,15 @@ class SignUpView(ListView):
     """User registration logic"""
     form = UserRegistrationForm()
     template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:signin')
 
     def post(self, request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse_lazy('accounts:signin'))
+        else:
+            print(form.errors)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -28,27 +32,43 @@ class SignUpView(ListView):
         pass
 
 
-class SignInView(ListView):
+# class SignUpView(CreateView):
+#     form = UserRegistrationForm()
+#     template_name = 'accounts/signup.html'
+#     success_url = reverse_lazy('accounts:signin')
+#
+#     def get_queryset(self):
+#         pass
+
+
+class SignInView(View):
     """User login logic"""
+    form_class = UserLoginForm
     template_name = 'accounts/signin.html'
 
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
     def post(self, request):
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user and user.is_active:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index-view'))
+        if request.method == "POST":
+            form = UserLoginForm(request, data=request.POST)
+            if form.is_valid():
+                username = request.POST['username']
+                password = request.POST['password']
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = UserLoginForm()
-        return context
+                user = auth.authenticate(username=username, password=password)
 
-    def get_queryset(self):
-        pass
+                if user and user.is_active:
+                    auth.login(request, user)
+                    return HttpResponseRedirect(reverse('index-view'))
+                else:
+                    messages.error(request, "Error")
+            else:
+                messages.error(request, "* Username or password incorrect")
+
+        form = UserLoginForm()
+        return render(request, "accounts/signin.html", {"form": form})
 
 
 # class ProfileView(ListView):
